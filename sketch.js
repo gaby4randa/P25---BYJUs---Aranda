24d5ae810714d9041fe301339fc154b32dbf8deb
gaby4randa <lunaorquiza1077@gmail.com>
2024-07-09 17:17:26 -0600

    sketch.js

--- sketch.js
+++ sketch.js
@@ -81,7 +81,7 @@ function draw() {
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(ball,ball.position,{x:20, y:-5});
+		Matter.Body.applyForce(ball,ball.position,{x:5, y:-50});
     
   	}
 }
